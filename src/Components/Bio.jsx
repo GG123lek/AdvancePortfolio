a5bccb35024fff333a01dfe97e96a3339ebf7bb4
GG123lek <ridw5613@gmail.com>
2025-01-22 15:40:00 +0100

    resume change

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -1,29 +1,42 @@
-import Heroimage from "../assets/gafarpictures2025.webp"
+import Heroimage from "../assets/gafarpictures2025.webp";
 
 function Bio() {
   return (
     <div className="bg-black text-white text-center py-16">
-      <img src={Heroimage} alt=''
-      className="mx-auto mb-8 w-48 h-48 rounded-full object-cover transform 
-      transition-transform duration-300 hover:scale-105"/>
+      <img
+        src={Heroimage}
+        alt=""
+        className="mx-auto mb-8 w-48 h-48 rounded-full object-cover transform 
+      transition-transform duration-300 hover:scale-105"
+      />
       <h1 className="text-4xl font-bold">
-        I am {" "}
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">Suleiman Gafar</span>
-        ,Front-end Developer
+        I am{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+          Suleiman Gafar
+        </span>
+        , Front-end Developer
       </h1>
-      <p className="mt-4 text-lg text-gray-300 ">
-        I specialised in building advance and responsive web applications
+      <p className="mt-4 text-lg text-gray-300">
+        I specialise in building advanced and responsive web applications.
       </p>
-      <div className="mt-8 space-x-4 ">
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
+      <div className="mt-8 space-x-4">
+        <button
+          className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
         transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
-        >Contact with me</button>
-        <button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline
+        >
+          Contact with me
+        </button>
+        <a
+          href="/path-to-resume/my-resume.pdf" // Replace with the actual path to your resume
+          download="Suleiman-Gafar-Resume.pdf" // This specifies the filename to save as
+          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline
         transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
-        >Resume</button>
+        >
+          Resume
+        </a>
       </div>
     </div>
-  )
+  );
 }
 
-export default Bio
+export default Bio;
