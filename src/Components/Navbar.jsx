ba62fb97ff1159e304a8c5c05024213f9b4b93f0
GG123lek <ridw5613@gmail.com>
2025-01-23 09:49:28 +0100

    connect done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,15 @@
 
 
 function Navbar() {
+
+  const handleConnect = () => {
+    
+    window.open("https://facebook.com", "_blank");
+    window.open("https://twitter.com", "_blank");
+    window.open("https://instagram.com", "_blank");
+    window.open("https://linkedin.com", "_blank");
+  };
+
   return (
     <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
       <div className="container py-2 flex justify-center md:justify-between items-center">
@@ -12,8 +21,17 @@ function Navbar() {
             <a href="#projects" className="hover:text-gray-400">Projects</a>
             <a href="#contact" className="hover:text-gray-400">Contacts</a>
         </div>
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
-        transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">Connect Me</button>
+        {/* <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
+        transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">Connect Me</button> */}
+              <div className="text-center mt-16">
+            <h1 className="text-2xl font-bold mb-4">Connect Me</h1>
+            <button
+              onClick={handleConnect}
+              className="bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-3 rounded-full transform transition-transform duration-300 hover:scale-105"
+            >
+              Connect Me
+            </button>
+          </div>
       </div>
     </nav>
   )
