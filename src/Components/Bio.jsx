6d2dee1c65649ef82ccefb7e840ce558343d6d47
GG123lek <ridw5613@gmail.com>
2025-01-23 09:40:53 +0100

    working

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -50,7 +50,7 @@ function Bio() {
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline
           transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
         >
-          Upload File
+          Resume
         </button>
 
         {/* Hidden file input */}
